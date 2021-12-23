3328a2bddd2788c2806a9de4027c8f9caf183429
Claudio <claudiohcrj@gmail.com>
2021-12-23 14:46:53 -0300

    Inicio do projeto Alteração de tela MainAct e AjustFreqResp 23-12

--- app/src/main/java/com/example/fisioctihelp/MainActivity.java
+++ app/src/main/java/com/example/fisioctihelp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.fisioctihelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAjusFreqResp, buttonAjusFIO2, tmpbuttonAjusFreqResp, tmpbuttonAjusFIO2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tmpbuttonAjusFreqResp = findViewById(R.id.buttonAjusFreqResp);
+        tmpbuttonAjusFreqResp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivityAjusFreqResp.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonAjusFIO2 = findViewById(R.id.buttonAjusFIO2);
+        buttonAjusFIO2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, MainActivityAjusFreqResp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
