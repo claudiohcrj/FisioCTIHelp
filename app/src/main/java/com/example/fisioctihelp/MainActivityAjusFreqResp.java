bf8e07b7a157c8c0ae527db3e5b0b51c843e3766
Claudio <claudiohcrj@gmail.com>
2021-12-24 14:24:34 -0300

    Inicio do projeto Alteração de tela MainAct e AjustFreqResp 24-12

--- app/src/main/java/com/example/fisioctihelp/MainActivityAjusFreqResp.java
+++ app/src/main/java/com/example/fisioctihelp/MainActivityAjusFreqResp.java
@@ -27,25 +27,25 @@ public class MainActivityAjusFreqResp extends AppCompatActivity {
         binding = ActivityMainAjusFreqRespBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        //setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_activity_ajus_freq_resp);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+       // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_activity_ajus_freq_resp);
+      //  appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+      //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+       // binding.fab.setOnClickListener(new View.OnClickListener() {
+      //      @Override
+       //     public void onClick(View view) {
+       //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+       //                 .setAction("Action", null).show();
             }
-        });
-    }
+      //  });
+   // }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_activity_ajus_freq_resp);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+   // @Override
+  //  public boolean onSupportNavigateUp() {
+    //    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_container);
+     //   return NavigationUI.navigateUp(navController, appBarConfiguration)
+     //           || super.onSupportNavigateUp();
+   // }
 }
