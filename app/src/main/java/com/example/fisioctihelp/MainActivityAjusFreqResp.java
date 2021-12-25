87587d12707f5738dff818e0694935ce2cb705b5
Claudio <claudiohcrj@gmail.com>
2021-12-25 12:05:37 -0300

    Inicio do projeto Alteração de tela MainAct e AjustFreqResp 24-12 Ativando funcionalidades tela AjustFreqResp 25-12

--- app/src/main/java/com/example/fisioctihelp/MainActivityAjusFreqResp.java
+++ app/src/main/java/com/example/fisioctihelp/MainActivityAjusFreqResp.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,7 +19,10 @@ import com.example.fisioctihelp.databinding.ActivityMainAjusFreqRespBinding;
 
 public class MainActivityAjusFreqResp extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+    EditText editTextfatual, editTextPaCO2Atual, editTextPaCO2Alvo;
+    TextView textViewResultado;
+
+    //private AppBarConfiguration appBarConfiguration;
     private ActivityMainAjusFreqRespBinding binding;
 
     @Override
@@ -26,6 +31,22 @@ public class MainActivityAjusFreqResp extends AppCompatActivity {
 
         binding = ActivityMainAjusFreqRespBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        editTextfatual = findViewById(R.id.editTextfatual);
+        editTextPaCO2Atual = findViewById(R.id.editTextPaCO2Atual);
+        editTextPaCO2Alvo = findViewById(R.id.editTextPaCO2Alvo);
+
+        textViewResultado = findViewById(R.id.textViewResultado);
+    }
+
+    public void buttonCalcFreqResp (View view) {
+        double fatual = Double.parseDouble(editTextfatual.getText().toString());
+        double paco2atual = Double.parseDouble(editTextPaCO2Atual.getText().toString());
+        double paco2alvo = Double.parseDouble(editTextPaCO2Alvo.getText().toString());
+        double resultado = fatual * paco2atual / paco2alvo;
+        textViewResultado.setText("Resultado:" + resultado);
+    }
+}
 
         //setSupportActionBar(binding.toolbar);
 
@@ -38,7 +59,7 @@ public class MainActivityAjusFreqResp extends AppCompatActivity {
        //     public void onClick(View view) {
        //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
        //                 .setAction("Action", null).show();
-            }
+
       //  });
    // }
 
@@ -48,4 +69,3 @@ public class MainActivityAjusFreqResp extends AppCompatActivity {
      //   return NavigationUI.navigateUp(navController, appBarConfiguration)
      //           || super.onSupportNavigateUp();
    // }
-}
