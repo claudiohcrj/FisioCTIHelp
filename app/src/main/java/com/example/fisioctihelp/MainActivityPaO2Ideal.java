e066a2a055042ae5eed3bd7958accf246f410ce5
Claudio <claudiohcrj@gmail.com>
2021-12-25 19:11:49 -0300

    Inicio do projeto Alteração de tela MainAct e AjustFreqResp 24-12 Ativando funcionalidades tela AjustFreqResp 25-12 Nova tela Ajuste da FIO2 25-12 Nova tela PaO2 Ideal

---
+++ app/src/main/java/com/example/fisioctihelp/MainActivityPaO2Ideal.java
@@ -0,0 +1,14 @@
+package com.example.fisioctihelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityPaO2Ideal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pa_o2_ideal);
+    }
+}
